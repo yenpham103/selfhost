7d76c56cda953c9fc4ac7cd19d5a3587b00d8986
Yen Px <yenpx.bsscommerce.com>
2025-06-12 21:46:56 +0700

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,107 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+
+export default {
+  title: 'Easto Subscriptions',
+  tagline: 'Documentation for Easto Subscriptions',
+  favicon: 'img/favicon.ico',
+  url: 'https://docs.easto.vn',
+  baseUrl: '/',
+  trailingSlash: false,
+  organizationName: 'yenpham103',
+  projectName: 'easto-subscriptions',
+  i18n: {
+    defaultLocale: 'vi',
+    locales: ['vi'],
+  },
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/yenpham103/easto-subscriptions/edit/main/',
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl: 'https://github.com/yenpham103/easto-subscriptions/edit/main/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+  themeConfig: {
+    navbar: {
+      title: 'Easto Subscriptions',
+      logo: {
+        alt: 'Easto Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Hướng dẫn',
+        },
+        { to: '/blog', label: 'Blog', position: 'left' },
+        {
+          href: 'https://github.com/yenpham103/easto-subscriptions',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Hướng dẫn',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/yenpham103/easto-subscriptions',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Easto Subscriptions, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+      additionalLanguages: ['bash', 'json'],
+    },
+  },
+};
